c14d2543740db0187ee302f229e604540cc64de2
Derin Saracoglu <64663540+derinsaracoglu@users.noreply.github.com>
2022-09-13 23:53:30 +0300

    Update endUp.java

--- Warmup-1/endUp.java
+++ Warmup-1/endUp.java
@@ -8,12 +8,12 @@ endUp("hi") → "HI" */
 public String endUp(String str) {
   if(str.length() <= 3){
     return str.toUpperCase();
-    
+  }
     int cut = str.length()-3;
     
     String front =str.substring(0,cut);
     String back = str.substring(cut);
     
     return front +back.toUpperCase();
-  }
+  
 }
